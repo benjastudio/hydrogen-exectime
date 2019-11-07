881027b5187e53599a3189b32cf41ad53718da51
Benjamin Blanc <benjam.blanc@gmail.com>
2019-11-07 16:02:40 +0100

    First commit

---
+++ spec/hydrogen-exectime-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HydrogenExectimeView from '../lib/hydrogen-exectime-view';
+
+describe('HydrogenExectimeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
